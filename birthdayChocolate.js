be03f23bcb0ba79138fd3be270f77ab75a0212f1
ozbekleromer <45530887+ozbekleromer@users.noreply.github.com>
2020-09-16 19:02:50 +0200

    Create birthdayChocolate.js

---
+++ birthdayChocolate.js
@@ -0,0 +1,59 @@
+/*
+HackerRank Task Explanation Page : https://www.hackerrank.com/challenges/the-birthday-bar/problem
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+    let sum = s.filter((val, i) => i < m).reduce((total, val,) => total + val);
+    let count = sum == d ? 1 : 0;
+    console.log(sum);
+    for(let i=m; i<s.length; i++) {
+        sum = sum + s[i] - s[i-m]
+        if(sum == d) count++;
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
