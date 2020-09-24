bcbbff8275c875bd8016c02269ba055e236152ce
ozbekleromer <45530887+ozbekleromer@users.noreply.github.com>
2020-09-24 22:49:15 +0200

    Create sockMerchant.js

---
+++ sockMerchant.js
@@ -0,0 +1,56 @@
+/*
+HackerRank Task Explanation Page : https://www.hackerrank.com/challenges/sock-merchant/
+*/
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let count = 0;
+    while(ar.length > 0) {
+        let lastSock = ar.lastIndexOf(ar[0]);
+        if(lastSock != 0) {
+            count++;
+            ar.splice(lastSock, 1);
+        }
+        ar.shift();
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
